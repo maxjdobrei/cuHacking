2754dbe5c6359532901e1abbf7719f3f9fb71c52
vivekpchand <44710731+vivekpchand@users.noreply.github.com>
2019-02-16 13:28:09 -0500

    Update Restrictions.py

--- Restrictions.py
+++ Restrictions.py
@@ -4,6 +4,14 @@ class Restrictions:
   intensity=[]
 
   def __init__(self,timeofday,breakTime,intensity):
-      self.timeofday= timeofday 
+      self.timeofday= timeofday
       self.breakTime= breakTime.split(":")
       self.intensity=intensity
+  def getTimeofDay(self):
+      return self.timeofday
+
+  def getbreakTime(self):
+        return self.breakTime
+
+  def intensity(self):
+      return self.intensity
