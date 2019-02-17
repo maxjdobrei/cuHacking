6e7b60aa380131ffb1bbbd936c927a3400f2aaa8
vivekpchand <44710731+vivekpchand@users.noreply.github.com>
2019-02-17 01:03:57 -0500

    Update Restrictions.py

--- Restrictions.py
+++ Restrictions.py
@@ -6,11 +6,8 @@ class Restrictions:
   def __init__(self,timeofday,breakTime,intensity):
 
       self.timeofday= timeofday
-      if breakTime != "":
-        workingBreakTime=breakTime.split(":")
-        self.breakTime=(workingBreakTime,(workingBreakTime[0]+1,workingBreakTime[1]))
-      else:
-        self.breakTime=99
+      workingBreakTime=breakTime.split(":")
+      self.breakTime=(workingBreakTime,(workingBreakTime[0]+1,workingBreakTime[1]))
       self.intensity=intensity
 
   def getTimeofDay(self):
