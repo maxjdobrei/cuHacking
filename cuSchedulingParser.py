454f4339b84b9f125f2c667d67ce6e834182029a
Shibbler <43223673+Shibbler@users.noreply.github.com>
2019-02-17 00:18:41 -0500

    Update cuSchedulingParser.py

--- cuSchedulingParser.py
+++ cuSchedulingParser.py
@@ -6,87 +6,6 @@ from Lecture import *
 from Tutorial import *
 from Restrictions import *
 
-
-
-# class Lecture:
-
-#     startTime=tuple()
-#     endTime=tuple()
-#     location=""
-#     days=[]
-#     tutorials=[]
-#     courseCode=""
-
-#     def __init__(self,courseCode,Time,location,day,tutorialsdays):
-#         Start= Time[0].split(":")
-#         End= Time[1].split(":")
-#         self.courseCode=courseCode
-#         self.startTime= (int(Start[0]), int(Start[1]))
-#         self.endTime= (int(End[0]), int(End[1]))
-#         self.location=location
-#         self.days= self.convertLectureDay(day)
-#         self.tutorials = tutorialsdays
-
-#     def toList(self):
-#         return [self.courseCode,self.startTime,self.endTime,self.location,self.days,self.tutorials]
-#     def convertLectureDay(self,day):
-#         dayNum=[]
-#         tempdays = day
-#         for days in tempdays:
-#             if days=="Mon":
-#                dayNum.append(0)
-#             elif days == "Tue":
-#                 dayNum.append(1)
-#             elif days == "Wed":
-#                 dayNum.append(2)
-#             elif days == "Thu":
-#                 dayNum.append(3)
-#             elif days == "Fri":
-#                 dayNum.append(4)
-#         return dayNum
-
-#     def getLectureTimes(self):
-# 	    return (self.startTime, self.endTime)
-
-# ############################################################################
-# class Tutorial:
-#     startTime=tuple()
-#     endTime=tuple()
-#     location=""
-#     days=[]
-#     courseCode=""
-#     def __init__(self,courseCode,Time,location,day):
-#         Start= Time[0].split(":")
-#         End= Time[1].split(":")
-
-#         self.courseCode=courseCode
-#         self.startTime= (int(Start[0]), int(Start[1]))
-#         self.endTime= (int(End[0]), int(End[1]))
-#         self.location=location
-#         self.days=self.convertDay(day)
-#     def __str__(self):
-#         return "Days of tuts " + self.days
-#     def convertDay(self,day):
-#         dayNum=[]
-#         tempdays = day
-#         for days in tempdays:
-#             if days=="Mon":
-#                 dayNum.append(0)
-#             elif days == "Tue":
-#                 dayNum.append(1)
-#             elif days == "Wed":
-#                 dayNum.append(2)
-#             elif days == "Thu":
-#                 dayNum.append(3)
-#             elif days == "Fri":
-#                 dayNum.append(4)
-#         return dayNum
-    
-#     def getTimes(self):
-# 	    return (self.startTime, self.endTime)
-
-#################################################################################################################
-
 class MegaList:
 
     def __init__(self):
@@ -249,7 +168,7 @@ def main(term,classes):
         crazyHugeList.append(temp)
     for item in crazyHugeList:
         if len(item) == 0:
-            return [-1]
+            return []
     return (crazyHugeList)
 
 
